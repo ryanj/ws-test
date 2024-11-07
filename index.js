e3680b04a96577b57f773193d5abfe2dcdae26e1
ryanj <ryan.jarvinen@gmail.com>
2024-11-07 11:37:08 -0800

    https support

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const app = express();
 const path = require('path');
-const expressWs = require('express-ws')(app);
 const port = process.env.PORT || 8080;
+var options = {};
+try{
+  options = {
+     key: fs.readFileSync(__dirname + '/private.key', 'utf8'),
+    cert: fs.readFileSync(__dirname + '/public.crt', 'utf8')
+  };
+  var server = https.createServer(options, app).listen(port);
+} catch (err){
+  console.error(err);
+  //const expressWs = require('express-ws')(app);
+  //app.listen(port);
+  var server = http.createServer(options, app).listen(port);
+}
+const expressWs = require('express-ws')(app, server);
 
 // Serve web page HTML
 app.get('/ws', (req, res) => {
@@ -24,5 +40,3 @@ app.ws('/ws/echo', (ws, req) => {
         wss.clients.forEach(client => client.send("Received: " + msg));
     })
 });
-
-app.listen(port);
