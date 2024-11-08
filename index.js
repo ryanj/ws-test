2bf9b1b5133a5460ad6371c9a4f77074645795d3
ryanj <ryan.jarvinen@gmail.com>
2024-11-08 06:26:48 -0800

    cleanup index.js

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ var options = {};
 try{
   options = {
      key: process.env['PRIVATE_KEY'] || fs.readFileSync(__dirname + '/private.key', 'utf8'),
-    cert: process.env['PUBLIC_CRT'] || fs.readFileSync(__dirname + '/public.crt', 'utf8')
+    cert: process.env['PUBLIC_CRT']  || fs.readFileSync(__dirname + '/public.crt',  'utf8')
   };
+  // certificates available
   console.log("protocol: https/wss")
 } catch (err){
   console.error(err);
+  // no certificates, fallback to http
   console.log("protocol: http/ws")
 }
-var protocol = ( Object.keys(options).length == 0 ) ? http : https;
-var server = protocol.createServer(options, app).listen(port);
+const protocol = ( Object.keys(options).length != 0 ) ? https : http;
+const server = protocol.createServer(options, app).listen(port);
 const expressWs = require('express-ws')(app, server);
 
 // Serve web page HTML
